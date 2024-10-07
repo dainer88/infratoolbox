ac2a47b0791fe3eaf278bc88b3cc2efe19f6c23f
GitHub Action <action@github.com>
2024-10-07 00:48:49 +0000

    Update Homebrew formule for release v0.1.0

--- infratoolbox.rb
+++ infratoolbox.rb
@@ -1,9 +1,9 @@
 class Infratoolbox < Formule
     desc "InfraToolbox integrates Terraform, Terraform Docs, TFEnv, TFLint, and other tools into a single CLI for managing infrastructure efficiently."
     homepage "https://github.com/dainer88/infratoolbox"
-    url "https://github.com/dainer88/infratoolbox/releases/download/refs/tags/v0.1.0/infratoolboxv0.1.0.tar.gz"
+    url "https://github.com/dainer88/infratoolbox/releases/download/refs/tags/v0.1.0/infratoolbox-v0.1.0.tar.gz"
     version "0.1.0"
-    sha256 "fffd8020a127141586474d0095b33a8682dc4f0eebab37018bc96ccb45dbd0d1"
+    sha256 "fe9974ded1f29df452c772c6917239af5d1d62263000a5c099a3598ff62a1de8"
 
     def install
         bin.install "infratoolbox"
