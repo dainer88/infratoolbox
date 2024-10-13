4d321688b853deec04198542c9c64991a64c09b7
GitHub Action <action@github.com>
2024-10-13 19:03:47 +0000

    Update Homebrew formule for release v0.1.0

--- infratoolbox.rb
+++ infratoolbox.rb
@@ -3,7 +3,7 @@ class Infratoolbox < Formula
     homepage "https://github.com/dainer88/infratoolbox"
     url "https://github.com/dainer88/infratoolbox/releases/download/v0.1.0/infratoolbox-v0.1.0.tar.gz"
     version "0.1.0"
-    sha256 "a6ce7dc51a530b04f09cff2f0ea13f8a383ec5bb77a28f9ccedf7536f6e43445"
+    sha256 ""
 
     def install
         bin.install "infratoolbox"
