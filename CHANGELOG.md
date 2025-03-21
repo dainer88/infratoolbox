5769ce4cad55269a91ea596a6d0c6f239c950c75
Dainer Mesa <dmesa8808@gmail.com>
2025-03-21 16:35:01 -0300

    Update CHANGELOG and infratoolbox.rb for version 0.2.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,19 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.2.0] - 2025-03-21
+
+### Added
+
+- **Terragrunt Integration**: Added support for running Terragrunt commands alongside Terraform.
+- **Auto-completion**: Implemented shell auto-completion for Bash, Zsh, Fish, and PowerShell.
+- **Merge Command Enhancements**: Improved the `merge` command to ensure files end with a blank line before merging.
+
+### Changed
+
+- **Documentation Updates**: Updated the `README.md` to reflect the latest features and commands.
+- **Cross-platform Installation**: Updated `install.sh` to support dependency installation on macOS, Linux, and Windows.
+
 ## [v0.1.0] - 2024-10-06
 
 ### Added
--- infratoolbox.rb
+++ infratoolbox.rb
@@ -1,8 +1,8 @@
 class Infratoolbox < Formula
     desc "InfraToolbox integrates Terraform, Terraform Docs, TFEnv, TFLint, and other tools into a single CLI for managing infrastructure efficiently."
     homepage "https://github.com/dainer88/infratoolbox"
-    url "https://github.com/dainer88/infratoolbox/releases/download/v0.1.0/infratoolbox-v0.1.0-macos.tar.gz"
-    version "v0.1.0"
+    url "https://github.com/dainer88/infratoolbox/releases/download/v0.2.0/infratoolbox-v0.2.0-macos.tar.gz"
+    version "v0.2.0"
     sha256 "bdc6b2066e9ca7dd5ce01aeb15b2b73d6cacc3d5f9c29615a0355543852f5a25"
 
     def install
@@ -14,6 +14,7 @@ class Infratoolbox < Formula
     end
 
     depends_on "terraform-docs"
+    depends_on "terragrunt"
     depends_on "tfenv"
     depends_on "tflint"
     depends_on "checkov"
